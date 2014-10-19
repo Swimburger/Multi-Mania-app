e4c0cbefccd507effa9c8bba9de3b100f926e676
Axel Jonckheere <solvesoul@gmail.com>
2014-10-19 14:06:27 +0200

    Added credentials + made online database

--- backend/Utilities/Params.php
+++ backend/Utilities/Params.php
@@ -10,12 +10,12 @@ namespace Utilities;
 
 
 class Params {
-    const PRODUCTION=false;
+    const PRODUCTION=true;
     public static function getConnectionParams(){
         if(Params::PRODUCTION){
-            return array("string"=>"mysql:host=localhost;dbname=multimania",
-                "username"=>"root",
-                "pwd"=>"root");
+            return array("string"=>"mysql:host=student.howest.be/axel.jonckheere/mysqlstudent;dbname=axeljonckheere",
+                "username"=>"axeljonckheere",
+                "pwd"=>"Apo8aisool0j");
         }else{
             return array("string"=>"mysql:host=localhost;dbname=multimania",
                 "username"=>"root",
