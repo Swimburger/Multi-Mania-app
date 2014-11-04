c53843cbaa06bae75492b5fe459c4fc164e69f49
Astrid D <astrid.desmedt@student.howest.be>
2014-11-04 17:21:46 +0100

    Update newsitem fragment & about fragment

--- app/MultiMania/app/src/main/java/be/ana/nmct/multimania/ui/AboutFragment.java
+++ app/MultiMania/app/src/main/java/be/ana/nmct/multimania/ui/AboutFragment.java
@@ -1,26 +1,78 @@
 package be.ana.nmct.multimania.ui;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.koushikdutta.ion.Ion;
 
 import be.ana.nmct.multimania.R;
 
 
 public class AboutFragment extends Fragment {
 
+    private Animation animFadein;
+    private Context context;
+    private ImageView imgAbout;
+    private TextView txtTitleEvent;
+    private TextView txtInfoEvent;
+    private TextView txtVenue;
+    private TextView txtStreet;
+    private TextView txtCity;
+    private TextView txtCountry;
+
     public AboutFragment() { }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about, container, false);
-    }
 
+        //animFadein = AnimationUtils.loadAnimation(context.getApplicationContext(), R.anim.fade_in);
+
+        View v = inflater.inflate(R.layout.fragment_about, container, false);
+        imgAbout = (ImageView)v.findViewById(R.id.imgAbout);
+        txtTitleEvent = (TextView)v.findViewById(R.id.txtAboutTitle);
+        txtInfoEvent = (TextView)v.findViewById(R.id.txtAboutInfo);
+        txtVenue = (TextView)v.findViewById(R.id.txtNameVenue);
+        txtStreet = (TextView)v.findViewById(R.id.txtStreet);
+        txtCity = (TextView)v.findViewById(R.id.txtCity);
+        txtCountry = (TextView)v.findViewById(R.id.txtCountry);
+
+
+        txtTitleEvent.setText(R.string.title_about);
+        txtInfoEvent.setText(R.string.about_info);
+        txtVenue.setText(R.string.name_venue);
+        txtStreet.setText(R.string.street);
+        txtCity.setText(R.string.city);
+        txtCountry.setText(R.string.country);
+
+        Drawable drawable = getResources().getDrawable(R.drawable.ic_multimania);
+      //  imgAbout.setImageDrawable(drawable);
+        imgAbout.setImageDrawable(drawable);
+        /*
+        Ion.with(imgAbout)
+                //.animateLoad()
+
+                //.resize(view.getMeasuredWidth(),
+                //        holder.imgNews.getHeight())
+                //.centerCrop()
+                .smartSize(true)
+                .animateIn(animFadein)
+                .load(drawable.toString());
+*/
+        return v;
+    }
 }
--- app/MultiMania/app/src/main/java/be/ana/nmct/multimania/ui/NewsItemFragment.java
+++ app/MultiMania/app/src/main/java/be/ana/nmct/multimania/ui/NewsItemFragment.java
@@ -9,6 +9,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,27 +18,25 @@ import android.widget.TextView;
 
 import be.ana.nmct.multimania.R;
 import be.ana.nmct.multimania.data.MultimaniaContract;
+import be.ana.nmct.multimania.model.NewsItem;
 
 public class NewsItemFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
 
-    private CustomCursorAdapterNews mAdapter;
     private int mChosenNewsItem;
 
-    public NewsItemFragment(){}
+    public NewsItemFragment() {
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Bundle bundle = getArguments();
-        // mChosenNewsItem = bundle.getInt(NewsFragment.ARGS_NEWS);
-        if(getArguments() != null){
+        if (getArguments() != null) {
             mChosenNewsItem = getArguments().getInt(MultimaniaContract.NewsItemEntry.NEWSITEM_ID);
-            System.out.println(mChosenNewsItem);
         }
-        mAdapter=new CustomCursorAdapterNews(getActivity(),null,0);
-
     }
+
+
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
@@ -46,48 +45,11 @@ public class NewsItemFragment extends Fragment implements LoaderManager.LoaderCa
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        mAdapter.swapCursor(data);
+
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-        mAdapter.swapCursor(null);
-    }
-}
 
-
-class CustomCursorAdapterNews extends CursorAdapter {
-    private LayoutInflater mInflater;
-    //private final Context context;
-
-    public CustomCursorAdapterNews(Context context, Cursor c, int flags) {
-        super(context, c, flags);
-        //this.context = context;
-    }
-
-    @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        mInflater = LayoutInflater.from(context);
-
-        View v  = mInflater.inflate(R.layout.fragment_news_item, parent, false);
-
-        ViewHolder holder = new ViewHolder();
-        holder.txtTitle = (TextView) v.findViewById(R.id.txtNewsItemTitle);
-        v.setTag(holder);
-        return v;
-
-    }
-
-    @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        ViewHolder holder = (ViewHolder)view.getTag();
-        int titleCol = cursor.getColumnIndexOrThrow(MultimaniaContract.NewsItemEntry.TITLE);
-        String title = cursor.getString(titleCol);
-        holder.txtTitle.setText(title);
-
-    }
-
-    static class ViewHolder{
-        TextView txtTitle;
     }
 }
