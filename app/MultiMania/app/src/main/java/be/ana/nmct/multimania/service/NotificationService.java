c6feed0f86b6bf34f97faa98f8d86d003ff29159
Axel Jonckheere <solvesoul@gmail.com>
2014-11-16 11:15:03 +0100

    Added notificationservice

---
+++ app/MultiMania/app/src/main/java/be/ana/nmct/multimania/service/NotificationService.java
@@ -0,0 +1,15 @@
+package be.ana.nmct.multimania.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by Axel on 16/11/2014.
+ */
+public class NotificationService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
