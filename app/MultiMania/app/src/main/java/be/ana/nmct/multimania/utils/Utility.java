130d75f4d498da0a3e233596495b60f68f5c87ca
Axel Jonckheere <solvesoul@gmail.com>
2014-11-17 10:20:22 +0100

    Added functionality to the service

--- app/MultiMania/app/src/main/java/be/ana/nmct/multimania/utils/Utility.java
+++ app/MultiMania/app/src/main/java/be/ana/nmct/multimania/utils/Utility.java
@@ -4,6 +4,7 @@ import android.view.animation.AlphaAnimation;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -48,6 +49,25 @@ public class Utility {
     public static String getTimeString(Date date) throws ParseException {
         return  sTimeFormatter.format(date);
     }
+
+    public static boolean isSameDay(Date date1, Date date2) {
+        if (date1 == null || date2 == null) {
+            throw new IllegalArgumentException("The dates must not be null");
+        }
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+        return isSameDay(cal1.getTime(), cal2.getTime());
+    }
+
+    /*public static boolean isBetweenTimeRange(Date toCheck){
+
+        DateTime before = dateToCheck;
+        DateTime after = sdf.parse("07/05/2012 08:30");
+
+        return (before.getTime() < toCheck.getTime()) && after.getTime() > toCheck.getTime();
+    }*/
 
     public static AlphaAnimation getAlphaAnimation(float from, float to, long duration, long offset) {
         AlphaAnimation anim = new AlphaAnimation(from,to);
