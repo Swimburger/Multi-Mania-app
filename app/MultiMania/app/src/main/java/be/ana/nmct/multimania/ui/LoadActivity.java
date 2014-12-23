a90ade0c5fa85c13e955c105fe763eb16cfdc40d
Axel Jonckheere <solvesoul@gmail.com>
2014-12-23 21:39:22 +0100

    Improved the loadingactivity loadingindicator with ellipsis

--- app/MultiMania/app/src/main/java/be/ana/nmct/multimania/ui/LoadActivity.java
+++ app/MultiMania/app/src/main/java/be/ana/nmct/multimania/ui/LoadActivity.java
@@ -9,10 +9,14 @@ import android.content.IntentFilter;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import be.ana.nmct.multimania.R;
 import be.ana.nmct.multimania.service.SyncAdapter;
@@ -20,7 +24,8 @@ import be.ana.nmct.multimania.utils.SettingsUtil;
 import be.ana.nmct.multimania.utils.Utility;
 
 /**
- * This activity is to see when the application for the first time startup
+ * This activity is shown when the application starts for the first time.
+ * It then loads all the data in the background and redirects the player to the MainActivity when ready.
  */
 public class LoadActivity extends Activity {
     private static final String TAG = LoadActivity.class.getSimpleName();
@@ -28,11 +33,15 @@ public class LoadActivity extends Activity {
     public static final String PREFERENCE_NAME = "launch_values";
     public static final String PREFERENCE_FIRSTTIMELAUNCH = "first_time_launch";
 
-    // Instance fields
-    Account mAccount;
+   private Account mAccount;
+   private AnimationDrawable mAnimation;
 
-    AnimationDrawable animation;
-    TextView txtLoading;
+   private TextView txtLoading;
+
+   private Timer mLoadingTimer;
+   private TimerTask mTimerTask;
+   private Handler mHandler;
+
 
     public static final String LOAD_SYNC = "be.ana.nmct.multimania.SYNCCOMPLETE";
 
@@ -45,18 +54,16 @@ public class LoadActivity extends Activity {
         image.setBackgroundResource(R.drawable.gifloader);
 
         txtLoading = (TextView)findViewById(R.id.txtLoading);
+        mHandler = new Handler();
 
-        txtLoading.setText(getString(R.string.loading));
-
-        animation = (AnimationDrawable)image.getBackground();
-        animation.start();
+        mAnimation = (AnimationDrawable)image.getBackground();
+        mAnimation.start();
 
         //Beautify for Lollipop users
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             getWindow().setStatusBarColor(getResources().getColor(R.color.primaryColorDark));
         }
-
 
         mAccount = Utility.getSyncAccount(this);
         Utility.requestSync(mAccount,false);
@@ -68,6 +75,39 @@ public class LoadActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        mLoadingTimer = new Timer();
+        initializeTimerTask();
+        mLoadingTimer.schedule(mTimerTask, 0, 500);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopTimerTask();
+    }
+
+    private void initializeTimerTask() {
+        mTimerTask = new TimerTask() {
+            @Override
+            public void run() {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(!txtLoading.getText().toString().contains("...")){
+                            txtLoading.setText(txtLoading.getText() + ".");
+                        } else {
+                            txtLoading.setText(getString(R.string.loading));
+                        }
+                    }
+                });
+            }
+        };
+    }
+
+    private void stopTimerTask(){
+        if(mLoadingTimer != null){
+            mLoadingTimer.cancel();
+        }
     }
 
     private BroadcastReceiver syncCompleteReceiver = new BroadcastReceiver() {
@@ -75,7 +115,7 @@ public class LoadActivity extends Activity {
         public void onReceive(Context context, Intent intent) {
             Log.d(TAG, "Sync ready received");
             //context.startActivity(MainActivity.class);
-            animation.stop();
+            mAnimation.stop();
 
             SettingsUtil launchUtil = new SettingsUtil(LoadActivity.this, PREFERENCE_NAME);
             launchUtil.setPreference(PREFERENCE_FIRSTTIMELAUNCH, false);
